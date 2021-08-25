9d0df64bc11b8722bb3d7094792aaeffe6c777e9
Furentsu <eugeniofranzoso@gmail.com>
2021-08-25 16:48:09 +0200

    Added two solutions

--- js/script.js
+++ js/script.js
@@ -1,22 +1,59 @@
-// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero 
-// e per i multipli di 5 stampi "Buzz". 
-// Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
+// SOLUZIONE #1
 
-let range = [];
+// let range = [];
 
-for (let i = 0; i < 101; i++) {
-    range.push(i);
-    if (range[i] % 3 == 0 && range[i] % 5 == 0) {
-        range[i] = "FizzBuzz";
-    } else if (range[i] % 3 == 0) {
-        range[i] = "Fizz";
-    } else if (range[i] % 5 == 0) {
-        range[i] = "Buzz";
+// for (let i = 0; i < 101; i++) {
+//     range.push(i);
+//     if (range[i] % 3 == 0 && range[i] % 5 == 0) {
+//         range[i] = "FizzBuzz";
+//     } else if (range[i] % 3 == 0) {
+//         range[i] = "Fizz";
+//     } else if (range[i] % 5 == 0) {
+//         range[i] = "Buzz";
+//     }
+// }
+
+// range.shift();
+// console.log(range);
+
+// SOLUZIONE #2 --------------------------------
+
+// let range = [];
+
+// for (let i = 1; i < 101; i++) {
+//     range.push(i);
+// }
+
+// console.log(range);
+
+// for (let i = 0; i < range.length; i++) {
+//     if (range[i] % 3 == 0 && range[i] % 5 == 0) {
+//         range[i] = "FizzBuzz";
+//     }   else if (range[i] % 3 == 0) {
+//         range[i] = "Fizz";
+//     }   else if (range[i] % 5 == 0) {
+//         range[i] = "Buzz";
+// }
+// }
+
+// console.log(range);
+
+// SOLUZIONE #3 --------------------------------
+
+for (let i = 1; i < 101; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
     }
 }
 
-range.shift();
-console.log(range);
+
+
 
 
 
