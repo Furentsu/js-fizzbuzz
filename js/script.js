f465b611aaf131956a0673430e6b6d1c91aaec31
Furentsu <eugeniofranzoso@gmail.com>
2021-08-25 16:09:38 +0200

    Solution (array)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,26 @@
 // e per i multipli di 5 stampi "Buzz". 
 // Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
 
-console.log("hello")
+let range = [];
+
+for (let i = 0; i < 101; i++) {
+    range.push(i);
+    if (range[i] % 3 == 0 && range[i] % 5 == 0) {
+        range[i] = "FizzBuzz";
+    } else if (range[i] % 3 == 0) {
+        range[i] = "Fizz";
+    } else if (range[i] % 5 == 0) {
+        range[i] = "Buzz";
+    }
+}
+
+range.shift();
+console.log(range);
+
+
+
+
+
+
 
 
